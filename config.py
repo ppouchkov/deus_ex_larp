d7ca42064fdf652dccdcbff9f520007b6c0c0bdd
Пучков Павел <ppuchkov@avito.ru>
2017-07-21 20:59:20 +0300

    config

---
+++ config.py
@@ -0,0 +1,23 @@
+import ConfigParser
+from collections import namedtuple
+
+CONFIG_PATH = 'config'
+
+config_parser = ConfigParser.RawConfigParser()
+config_parser.read(CONFIG_PATH)
+
+JabberUser = namedtuple('JabberUser', ['jid', 'pwd'])
+
+attacker = JabberUser(
+    config_parser.get('attacker', 'jid'),
+    config_parser.get('attacker', 'pwd'),
+)
+
+node_holder = JabberUser(
+    config_parser.get('node_holder', 'jid'),
+    config_parser.get('node_holder', 'pwd'),
+)
+
+data = config_parser.get('global', 'data_folder')
+
+print node_holder
